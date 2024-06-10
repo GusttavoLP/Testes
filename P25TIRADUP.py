5287c43ee336bfb268d49bde063e27d062ce829c
GusttavoLP <95048786+GusttavoLP@users.noreply.github.com>
2024-06-09 22:25:46 -0300

    Update P25TIRADUP.py

--- P25TIRADUP.py
+++ P25TIRADUP.py
@@ -19,16 +19,16 @@ def gervetint (n,inic,fin,f):
     print(f)
     from random import randint
     return([randint(inic,fin) for i in range(0,n,1)])
-def locdup (V,f):
+def locdup (V,f,V2):
     print(f)
+    M=0
     V2=[]
-    for i in range (0,(len(V)),1):
-        naoFoi = True
-        for j in range (0,(len(V)),1):
-            if((V[i])==(V[j]) and (i!=j) and naoFoi):
-                V2.append(i)
-                naoFoi = False
-    return V2
+    for i in range (1,(len(V)-1),1):
+        if(V(i)==V(M)):
+            V2.append(i)
+        else:
+            M=M+1
+    return( )
 #
 #BOAS VINDAS
 print("Bem vindo, Sr. usuário!")
@@ -36,36 +36,21 @@ print("PROGRAMA ELIMINA DUPLICATAS DE UM VETOR")
 print(" ")
 #
 #DIÁLOGO DE ENTRADA DE DADOS
-X=gervetint(100,200,400,"Gerando vetor X.")
+X=gervetint(99,200,400,"Gerando vetor X.")
 print("Vetor X gerado com",len(X),"elementos:\n",X)
 #
 #PROCESSAMENTO
-X2 = locdup(X,"Procurando duplicatas em X...")
-print("As duplicatas de X estão localizadas nas posições:\n",X2)      
-qtdDup = len(X2)
-X3 = X.copy()
-X4 = []
-i = (len(X2)-1)
-while (len(X2))>0:                          #enquanto houverem duplicatas
-    X3.pop(X2[i])                           #Remove de X a duplicata da posição
-    if (len(X4)!=0):
-        jaTem = False
-        for j in range (0,(len(X4)),1):         #percorre X4 para saber se já registrou aquela duplicata
-            if (X[X2[i]]==(X4[j])):             #se ja tiver a primeira ocorrencia registrada ele não registra
-                jaTem = True
-        if not(jaTem):
-            X4.append(X[X2[i]])                 #se não tiver registrado, registra
-    else:
-        X4.append(X[X2[i]])                     #primeiro registro sempre é feito
-    X2.pop()    
-    i -= 1                                  #Uso de while pois o vetor muda de tamanho       
-for k in range (0,(len(X4)),1):             #adiciona novamente a primeira ocorrencia de cada duplicata
-    X3.append(X4[k])
+locdup(X,"Procurando duplicatas em X...",X2)
+print("As duplicatas de X estão localizadas nas posições:\n",X2)
+while (len(X2))>1:                         #enquanto houverem duplicatas
+    for i in range (len(X2),1,-1):
+        X.pop(X2[i])                       #Remove de X a duplicata da posição
+    X2.pop( )                              #Uso de while pois o vetor muda de tamanho
 #
 #
 #EMISSÃO DO RELATÓRIO DE SAÍDA
-print("O vetor X continha", qtdDup,"duplicatas.")
-print("Vetor X sem duplicatas:\n",X3)
+print("O vetor X continha",(len(X2)-1),"duplicatas.")
+print("Vetor X sem duplicatas:\n",X)
 print(" ")
 #
 #DESPEDIDA
